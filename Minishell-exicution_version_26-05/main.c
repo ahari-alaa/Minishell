60bd309a7d829a0c634261a78c3661b9d7b2e6ae
MOHAMED ASKOUR <116392756+m-askour@users.noreply.github.com>
2025-05-26 22:18:06 +0100

    befor the last virsion

---
+++ Minishell-exicution_version_26-05/main.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maskour <maskour@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 15:08:30 by maskour           #+#    #+#             */
+/*   Updated: 2025/05/26 22:06:35 by maskour          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#include "minishell.h"
+// this main for marge
+int main(int ac,char **av,char **env)
+{
+    (void)ac;
+    (void)av;
+    char        *input;
+    t_token     *tokens;
+    t_cmd       *commands;
+    t_env *env_list;
+    env_list = file_inv(env);
+    signal(SIGINT, handler_sig); 
+    signal(SIGQUIT, handler_sig);   
+    while (1)
+    {
+        input = readline("minishell$ ");
+        if (!input)
+        {
+
+
+            
+            printf("exit\n");
+            break ;
+        }
+        if (*input)
+            add_history(input);
+        tokens = check_quoted(input);
+        if (!tokens)
+        {
+            free (input);
+            continue ;
+        }
+        commands = parse_commands(tokens);
+        if (!commands)
+        {
+            free_tokens(tokens, input);
+            continue ;
+        }
+        commands = expand_cmd_list(commands);
+        if (!commands)
+        {
+            free_cmd_list(commands);
+            free_tokens(tokens, input);
+            continue ;
+        }
+        commands = unquote_cmd_list(commands);
+        if (!commands)
+        {
+            free_cmd_list(commands);
+            free_tokens(tokens, input);
+            continue ;
+        }
+        if (commands)
+        {
+            t_cmd *current = commands;
+            int i = 1;
+            
+            while (current)
+            {
+                printf("Command #%d:\n", i);
+                print_command_with_files(current);
+                current = current->next;
+                i++;
+            }
+        }
+        exicut(&commands, env_list);
+        if(commands)
+            free_cmd_list(commands);
+        free_tokens(tokens, input);
+    }
+    //free the env
+    free_env_list(env_list);
+    // free_env_list(env_list);
+    return (0);
+}
+
