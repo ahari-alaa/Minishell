4fcfdb6da44bb551a73cf83d2e9aefb6e9920d58
Alaa Hari <ahari@e3r4p16.1337.ma>
2025-06-20 23:31:38 +0100

    done

---
+++ Minishell-exicution_version_01-06/exicution_minishell/builtin/ft_unset.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_unset.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maskour <maskour@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 16:51:36 by maskour           #+#    #+#             */
+/*   Updated: 2025/06/19 15:08:20 by maskour          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+t_env *ft_unset(t_cmd **cmd, t_env *env, t_shell *shell_ctx)
+{
+    t_env *current;
+    t_env *prev;
+    t_env *to_free;
+    int len;
+    int i = 1;
+    t_cmd *current_cmd = *cmd;
+    while (current_cmd->cmd[i])
+    {
+        len = ft_strlen(current_cmd->cmd[i]);
+        current = env;
+        prev = NULL;
+        while (current != NULL)
+        {
+            if(!ft_strncmp(current->data_env, current_cmd->cmd[i], len) && \
+                    current->data_env[len] == '=')
+            {
+                to_free = current;
+                if (prev == NULL)
+                    env = current->next;
+                else
+                    prev->next = current->next;
+                current = current->next;
+                free(to_free->data_env);
+                free(to_free);
+            }
+            else 
+            {
+                prev = current;
+                current = current->next;
+            }
+        }
+        i++;
+    }
+    shell_ctx->exit_status = 0;
+    return (env);
+}
---
+++ Minishell-exicution_version_01-06/main.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahari <ahari@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 15:08:30 by maskour           #+#    #+#             */
+/*   Updated: 2025/06/20 23:27:07 by ahari            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#include "minishell.h"
+
+
+void free_char_array(char **array)
+{
+    int i;
+
+    if (!array)
+        return;
+    i = 0;
+    while (array[i])
+    {
+        free(array[i]);
+        i++;
+    }
+    free(array);
+}
+
+int main(int ac,char **av,char **env)
+{
+    (void)ac;
+    (void)av;
+    char        *input;
+    t_token     *tokens;
+    t_cmd       *commands;
+    t_env *env_list;
+    env_list = file_inv(env);
+    signal(SIGINT, handler_sig); 
+    signal(SIGQUIT, handler_sig); 
+    t_shell *shell_ctx;
+    shell_ctx = malloc (sizeof(t_shell));
+    if(!shell_ctx)
+        exit(1);
+    shell_ctx->exit_status = 0; 
+    while (1)
+    {
+        input = readline("minishell$ ");
+        if (!input)
+        {            
+            write(1 ,"exit\n", 5);
+            break ;
+        }
+        if (*input)
+            add_history(input);
+        char **env_table = convert(env_list);
+        tokens = check_quoted(input, shell_ctx, env_table);
+        if (!tokens)
+        {
+            free (input);
+            free_char_array(env_table);
+            continue ;
+        }
+        commands = parse_commands(tokens , shell_ctx);
+        if (!commands)
+        {
+            free_tokens(tokens, input);
+            continue ;
+        }
+        commands = expand_cmd_list(commands, shell_ctx, env_table);
+        if (!commands)
+        {
+            free_cmd_list(commands);
+            free_tokens(tokens, input);
+            continue ;
+        }
+        exicut(&commands, &env_list, shell_ctx);
+        if(commands)
+            free_cmd_list(commands);
+        
+        free_tokens(tokens, input);
+        free_char_array(env_table);
+    }
+    free_env_list(env_list);
+    free(shell_ctx);
+    return (0);
+}
+
