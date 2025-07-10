dd9264abdcb6dd8f078b1e7a1841ea0b54cee547
MOHAMED ASKOUR <116392756+m-askour@users.noreply.github.com>
2025-07-10 16:29:34 +0100

    Update redirections.c

--- Minishell-exicution_version_23-06/exicution_minishell/redirections.c
+++ Minishell-exicution_version_23-06/exicution_minishell/redirections.c
@@ -30,7 +30,8 @@ static char *get_rundem_name(char *file_name) {
     char *count_str;
     char *tmp;
 
-    filename = ft_strdup(file_name);
+    // Use /tmp directory (no need to check, always exists)
+    filename = ft_strjoin("/tmp/minishell_", file_name);
     if (!filename) return NULL;
     fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
     if (fd != -1) { close(fd); return filename; }
@@ -40,11 +41,14 @@ static char *get_rundem_name(char *file_name) {
     while (count < 1000) {
         count_str = ft_itoa(count);
         if (!count_str) return NULL;
-        filename = ft_strjoin(file_name, "_");
-        if (!filename) { free(count_str); return NULL; }
-        tmp = ft_strjoin(filename, count_str);
-        free(filename);
+        tmp = ft_strjoin(file_name, "_");
+        if (!tmp) { free(count_str); return NULL; }
+        filename = ft_strjoin(tmp, count_str);
+        free(tmp);
         free(count_str);
+        if (!filename) return NULL;
+        tmp = ft_strjoin("/tmp/minishell_", filename);
+        free(filename);
         if (!tmp) return NULL;
         filename = tmp;
         fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
@@ -63,35 +67,92 @@ static int cleanup_stdio(int original_stdin, int original_stdout)
     return 1;
 }
 // ----- Heredoc: prompts user, writes to temp file, updates file->name -----
-int function_herdoc(t_file *file) {
+int function_herdoc(t_file *file, char **env, t_shell *shell_ctx)
+{
+    // Generate the temp filename BEFORE fork
     char *filename = get_rundem_name(file->name);
-    if (!filename) { perror("minishell: cannot create temporary file"); return 1; }
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd == -1) { perror("minishell: open"); free(filename); return 1; }
-
-    char *line;
-    printf("delimiter is : %s\n", file->name);
-    while (1) {
-        line = readline("> ");
-        if (!line) 
-            break;
-        if (!ft_strcmp(line, file->name)) { free(line); break; }
-        write(fd, line, strlen(line));
-        write(fd, "\n", 1);
-        free(line);
+    if (!filename) {
+        perror("minishell: cannot create temp file");
+        return 1;
     }
-    close(fd);
+    pid_t pid = fork();
+    if (pid == -1) {
+        free(filename);
+        perror("minishell: fork (heredoc)");
+        return 1;
+    }
+    signal(SIGINT, SIG_IGN);
+    if (pid == 0)
+    {
+        // Child process: set default signal handling
+        signal(SIGINT, SIG_DFL);
+        signal(SIGQUIT, SIG_DFL);
 
+        int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd == -1)
+        {
+            perror("minishell: open");
+            free(filename);
+            exit(1);
+        }
+
+        char *line;
+        while (1)
+        {
+            line = readline("> ");
+            if (!line)
+                break; // Normal exit on EOF
+
+            if (!ft_strcmp(line, file->name))
+            {
+                free(line);
+                break;
+            }
+
+            char *tmp = ft_strjoin("$", file->name);
+            if (file->check_expand == 0 && ft_strcmp(line, tmp) != 0)
+            {
+                char *expanded_line = found_env(line, env, shell_ctx);
+                if (!expanded_line)
+                {
+                    free(line);
+                    close(fd);
+                    unlink(filename);
+                    free(filename);
+                    exit(1);
+                }
+                write(fd, expanded_line, ft_strlen(expanded_line));
+                write(fd, "\n", 1);
+                free(expanded_line);
+            } 
+            else
+            {
+                write(fd, line, strlen(line));
+                write(fd, "\n", 1);
+            }
+            free(tmp);
+            free(line);
+        }
+        close(fd);
+        free(filename); // Not needed in child anymore
+        exit(0); // Success
+    }
+
+    // Parent: wait for heredoc process
+    int status;
+    waitpid(pid, &status, 0);
+
+    // Save the filename in the file struct (now parent and child agree)
     free(file->name);
-    file->name = filename; // Now redirections will open the right file
+    file->name = filename;
 
-    signal(SIGINT, handler_sig);
-    signal(SIGQUIT, handler_sig);
-    return 0;
+    if (WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
+        return (130); // Tell caller to cancel pipeline
+
+    return WEXITSTATUS(status);
 }
-
 // ----- Redirection logic -----
-int redirections(t_cmd *cmd)
+int redirections(t_cmd *cmd, char **env, t_shell *shell)
 {
     if (!cmd || cmd->file_count <= 0)
         return 0;
@@ -111,8 +172,10 @@ int redirections(t_cmd *cmd)
     while (++i < cmd->file_count)
     {
         if (cmd->files[i].type == TOKEN_HEREDOC) {
-            if (function_herdoc(&cmd->files[i]) != 0)
-                return cleanup_stdio(original_stdin, original_stdout);
+            if (function_herdoc(&cmd->files[i], env, shell))
+            {
+                cleanup_stdio(original_stdin, original_stdout);
+            }    
             last_heredoc_index = i;
         }
     }
@@ -124,30 +187,36 @@ int redirections(t_cmd *cmd)
             if (last_in_fd != -1) close(last_in_fd);
             last_in_fd = open_file(file->name, 0);
             if (last_in_fd == -1) {
-                perror("minishell1");
-                return cleanup_stdio(original_stdin, original_stdout);
+                perror("minishell: open_file input");
+                cleanup_stdio(original_stdin, original_stdout);
+                return 1;
             }
         }
         else if (file->type == TOKEN_HEREDOC && i == last_heredoc_index) {
             if (last_in_fd != -1) close(last_in_fd);
             last_in_fd = open(file->name, O_RDONLY);
-            if (last_in_fd == -1)
-                return cleanup_stdio(original_stdin, original_stdout);
+            if (last_in_fd == -1) {
+                perror("minishell: open heredoc file");
+                cleanup_stdio(original_stdin, original_stdout);
+                return 1;
+            }
         }
         else if (file->type == TOKEN_REDIRECT_OUT) {
             if (last_out_fd != -1) close(last_out_fd);
             last_out_fd = open_file(file->name, 1);
             if (last_out_fd == -1) {
-                perror("minishell");
-                return cleanup_stdio(original_stdin, original_stdout);
+                perror("minishell: open_file output");
+                cleanup_stdio(original_stdin, original_stdout);
+                return 1;
             }
         }
         else if (file->type == TOKEN_APPEND) {
             if (last_out_fd != -1) close(last_out_fd);
             last_out_fd = open_file(file->name, 2);
             if (last_out_fd == -1) {
-                perror("minishell");
-                return cleanup_stdio(original_stdin, original_stdout);
+                perror("minishell: open_file append");
+                cleanup_stdio(original_stdin, original_stdout);
+                return 1;
             }
         }
     }
@@ -155,9 +224,10 @@ int redirections(t_cmd *cmd)
     {
         if (dup2(last_in_fd, STDIN_FILENO) == -1)
         {
-            perror("minishell");
+            perror("minishell: dup2 input");
             close(last_in_fd);
-            return cleanup_stdio(original_stdin, original_stdout);
+            cleanup_stdio(original_stdin, original_stdout);
+            return 1;
         }
         close(last_in_fd);
     }
@@ -165,9 +235,10 @@ int redirections(t_cmd *cmd)
     {
         if (dup2(last_out_fd, STDOUT_FILENO) == -1)
         {
-            perror("minishell");
+            perror("minishell: dup2 output");
             close(last_out_fd);
-            return cleanup_stdio(original_stdin, original_stdout);
+            cleanup_stdio(original_stdin, original_stdout);
+            return 1;
         }
         close(last_out_fd);
     }
