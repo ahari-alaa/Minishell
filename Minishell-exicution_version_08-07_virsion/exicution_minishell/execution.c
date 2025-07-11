877cb46273c524f24a8ab45b8b8c4da62ad20ec3
MOHAMED ASKOUR <116392756+m-askour@users.noreply.github.com>
2025-07-11 16:42:16 +0100

    Update execution.c

--- Minishell-exicution_version_08-07_virsion/exicution_minishell/execution.c
+++ Minishell-exicution_version_08-07_virsion/exicution_minishell/execution.c
@@ -526,3 +526,64 @@ int exicut(t_cmd **cmd, t_env **env_list, t_shell *shell_ctx)
     free_env(env);
     return (0);
 }
+// static int count_cmd(t_cmd *cmd)
+// {
+//     int count;
+    
+//     count = 0;
+//     while (cmd)
+//     {
+//         count++;
+//         cmd = cmd->next;
+//     }
+//     return (count);
+// }
+// static t_cmd **convert_lit_arr(t_cmd *cmd, int cmd_count)
+// {
+//         t_cmd    *current = cmd;
+//         t_cmd **cmd_arr = malloc(sizeof(t_cmd *) * (cmd_count + 1));
+//         if (!cmd_arr)
+//             return (NULL);
+//         int i = -1;
+//         while ( ++i < cmd_count) 
+//         {
+//             cmd_arr[i] = current;
+//             current = current->next;
+//         }
+//         cmd_arr[cmd_count] = NULL;
+//         return (cmd_arr);
+// }
+// static void execute_single(t_cmd **cmd, char **env, t_env **env_list, t_shell *shell_ctx)
+// {
+//         if (is_builtin((*cmd)->cmd[0]))
+//             *env_list = execut_bultin(cmd, env,*env_list, shell_ctx, 1);
+//         else
+//             execute_single_command(cmd, env,shell_ctx);
+// }
+// int exicut(t_cmd **cmd, t_env **env_list, t_shell *shell_ctx)
+// {
+//     int cmd_count;
+//     t_cmd **cmd_arr;
+//     char **env;
+
+//     if (!cmd || !*cmd || !env_list)
+//         return (1);
+//     env = convert(*env_list);
+//     if (!env)
+//         return (1);
+//     cmd_count = count_cmd(*cmd);
+//     if (cmd_count == 1)
+//         execute_single(cmd, env, env_list, shell_ctx);
+//     else
+//     {
+//         cmd_arr = convert_lit_arr(*cmd, cmd_count);
+//         if (!cmd_arr) {
+//             free_env(env);
+//             return (1);
+//         }
+//         execute_pipeline(cmd_arr, cmd_count, env, *env_list,shell_ctx);
+//         free(cmd_arr);
+//     }
+//     free_env(env);
+//     return (0);
+// }
